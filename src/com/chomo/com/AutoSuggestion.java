bb4120ed4c653fe28b0e5cb65cd61d06327b3fc3
gyanesh <kamal.gyanesh8@gmail.com>
2018-04-29 02:43:26 +0530

    Changes in code

--- src/com/chomo/com/AutoSuggestion.java
+++ src/com/chomo/com/AutoSuggestion.java
@@ -13,10 +13,18 @@ public class AutoSuggestion
 	{
 		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
 		WebDriver  driver=new ChromeDriver();
+		driver.manage().deleteAllCookies();
 		driver.get("http://www.google.com");
 		driver.findElement(By.id("lst-ib")).sendKeys("selenium");
-		List<WebElement> allAST = driver.findElements(By.xpath("//div[@class='sbqs_c']"));
-		int count = allAST.size();
-		System.out.println(count);
+		List<WebElement> list = driver.findElements(By.xpath("//ul[@role='listbox']//li/descendant::div[@class='sbqs_c']"));
+		//int count = list.size();
+		System.out.println("total number of suggestions in search box:::===>" + list.size());
+		for(int i=0; i<list.size(); i++){
+			System.out.println(list.get(i).getText());
+			if(list.get(i).getText().contains("selenium")){
+				list.get(i).click();
+				break;
 }
+		}
+	}
 }
